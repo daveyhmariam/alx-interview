1870ea0a9552bfe8f7c84d1b0d132f834ae75dd7
daveyhmariam <daveyhmariam@gmail.com>
2024-08-15 23:54:59 +0300

    style fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,13 +3,14 @@
 List comprehnsions 2D matrix rotation
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotate a 2D matrix 90 degrees
 
     Args:
     matrix : n x n matrix
-    
+
     Returns:
     None: matrix modified in place.
     """
