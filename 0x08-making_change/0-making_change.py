9680a6e2e996bbb4b9ffac9759c26647ff6aaa05
daveyhmariam <daveyhmariam@gmail.com>
2024-08-22 01:31:19 +0300

    pycodestyle fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,26 +4,33 @@ Making change with given number of coins
 with the list number of coins
 """
 
-
 def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total
+    number of coins needed to meet a given amount total.
 
     Args:
-        coins (List): list of coin denomination
-        total (int): the total amount of change
+        coins (List[int]): List of coin denominations
+        total (int): The total amount of change
+
+    Returns:
+        int: Fewest number of coins needed to meet total, or -1 if not possible
     """
     if total <= 0:
         return 0
+
+    # Sort coins in descending order
     coins.sort(reverse=True)
-    for i in range(len(coins)):
-        c = coins[i:]
-        sum = 0
-        mod = total
-        for elm in c:
-            sum += (mod // elm)
-            mod %= elm
-            if mod == 0:
-                return sum
-    return -1
+
+    # Try to use the fewest number of coins
+    num_coins = 0
+    remaining_amount = total
+
+    for coin in coins:
+        if remaining_amount == 0:
+            break
+        num_coins += remaining_amount // coin
+        remaining_amount %= coin
+
+    # If we could not make the exact amount, return -1
+    return num_coins if remaining_amount == 0 else -1
