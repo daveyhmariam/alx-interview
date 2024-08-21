1be788ee2f109bdc500d48c37fc9b75cec3ba2d0
daveyhmariam <daveyhmariam@gmail.com>
2024-08-22 01:04:39 +0300

    Given a pile of coins of different values, determine the fewest     number of coins needed to meet a given amount total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,36 +4,26 @@ Making change with given number of coins
 with the list number of coins
 """
 
+
 def makeChange(coins, total):
-    """
+    """ 
     Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total.
+    number of coins needed to meet a given amount total
 
     Args:
-        coins (List[int]): List of coin denominations.
-        total (int): The total amount of change to make.
-
-    Returns:
-        int: Fewest number of coins needed to meet total, or -1 if not possible.
+        coins (List): list of coin denomination
+        total (int): the total amount of change
     """
     if total <= 0:
         return 0
-    
-    # Sort coins in descending order to use the largest denominations first
     coins.sort(reverse=True)
-    
-    # Initialize count of coins used
-    num_coins = 0
-    remaining_amount = total
-    
-    # Iterate over each coin denomination
-    for coin in coins:
-        if remaining_amount == 0:
-            break
-        
-        # Calculate how many coins of this denomination are needed
-        num_coins += remaining_amount // coin
-        remaining_amount %= coin
-    
-    # If remaining_amount is 0, we successfully made the change
-    return num_coins if remaining_amount == 0 else -1
+    for i in range(len(coins)):
+        c = coins[i:]
+        sum = 0
+        mod = total
+        for elm in c:
+            sum += (mod // elm)
+            mod %= elm
+            if mod == 0:
+                return sum
+        return -1
