a50318565e8ab84c5da7fc53bd928318b44d0289
daveyhmariam <daveyhmariam@gmail.com>
2024-09-05 16:33:21 +0300

    game theory, game play finding prime numbers

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,44 +6,57 @@ Sieve of Eratosthenes Algorithm
 
 def isWinner(x, nums):
     """
+    Determines the winner of a game based on prime numbers within given ranges.
 
     Args:
-        x (int): Number of rounds
-        nums (array): Contains ranges on which the game is played on
+        x (int): Number of rounds played.
+        nums (list): List of ranges (integers) on which the game is played.
 
-    Return (str): The name of the player won the most
+    Returns:
+        str: The name of the winning player ("Maria" or "Ben"),
+        or None if there's a tie.
     """
+
     if x == 0 or not nums:
-        return None
-    b = 0
-    m = 0
+        return None  # Handle empty input or zero rounds
+
+    maria_wins = 0
+    ben_wins = 0
+
     for i in nums:
-        p = findPrime(i)
-        if p % 2 == 1:
-            m += 1
+        prime_count = find_primes(i)
+        if prime_count % 2 == 1:
+            maria_wins += 1
         else:
-            b += 1
-    if m > b:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
-    elif m < b:
-        return 'Ben'
+    elif maria_wins < ben_wins:
+        return "Ben"
     else:
         return None
 
 
-def findPrime(n):
-    """Returns prime numbers upto n
+def find_primes(n):
+    """
+    Finds the number of prime numbers up to a given
+    limit using the Sieve of Eratosthenes.
 
     Args:
-        n (int): The range to find primes within
+        n (int): The upper limit for finding primes.
+
+    Returns:
+        int: The count of prime numbers found.
     """
-    n += 1
-    arr = [True] * n
-    primes = 0
-    for i in range(2, n):
-        if arr[i]:
-            primes += 1
-            if i * i < n:
-                for j in range(i * i, n, i):
-                    arr[j] = False
-    return primes
+
+    n += 1  # Include the limit itself
+    primes = [True] * n
+    primes[0] = primes[1] = False  # Mark 0 and 1 as non-prime
+
+    for i in range(2, int(n**0.5) + 1):  # Optimize iteration limit
+        if primes[i]:
+            for j in range(i * i, n, i):
+                primes[j] = False
+
+    return sum(primes)  # Count the remaining True values
